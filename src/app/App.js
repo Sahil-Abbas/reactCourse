99dc0308e69f073d4eef7e642cece237d2a2a87a
Sahil Abbas <sahil.abbas@auxilium.digital>
2022-05-29 12:25:47 +0500

    initial setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,48 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
 import './App.css';
-import Counter from './components/Counter';
-import HeaderComponent from './components/HeaderComponent';
-import LoginComponent from './components/LoginComponent';
-import MenuComponent from './components/MenuComponent';
-import OrderComponent from './components/OrderComponent';
-import OrderDetail from './components/OrderDetail';
+
 
 function App() {
-    const [posts, setPost] = useState([]);
-
-    // componentDidMount
-    // useEffect(() => {
-    //     fetch('https://jsonplaceholder.typicode.com/posts',
-    //         {
-    //             method: "GET"
-    //         })
-
-    //         .then((data) => data.json())
-
-    //         .then((response) => {
-    //             setPost(response)
-    //         }).catch((err) => {
-    //             console.log(err)
-    //         })
-    //     // componentWillUnMount
-    //     // componentDidUpdate
-    // }, [])
 
     return (
         <div className='App'>
-            <HeaderComponent />
-            <Routes>
-                <Route path='/' element={<div>Home</div>} />
-                <Route path='/menu' element={<MenuComponent />} />
-                <Route path='/orders' element={<OrderComponent />}>
-                    <Route index={true} element={<div>Select order</div>} />
-                    <Route path=':id' element={<OrderDetail />} />
-                </Route>
-                <Route path='/login' element={<LoginComponent />} />
-                <Route path='/counter' element={<Counter />} />
-                <Route path="*" element={<div>404</div>} />
-            </Routes>
+            Hello
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { loginState, login, logout, UserContext } from './app/context/UserContext';
-
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContext.Provider value={{loginState, login, logout}}>
-      <BrowserRouter>
-        <App></App>
-      </BrowserRouter>
-    </UserContext.Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
